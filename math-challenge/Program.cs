0805b2eddf5609694560167526b6e2ec15844f46
J-C <jcjoneskern@gmail.com>
2017-12-12 22:05:43 -0500

    some refactoring

--- math-challenge/Program.cs
+++ math-challenge/Program.cs
@@ -2,72 +2,69 @@
 
 public class NumberChecker
 {
-    public String getNumber()
-    {
-        String num;
+	public String getNumber()
+	{
+		String num;
 
-        Console.WriteLine("Enter a whole number:");
-        num = Console.ReadLine();
+		Console.WriteLine("Enter a whole number:");
+		num = Console.ReadLine();
 
-        while (!(Int32.TryParse(num, out int number)))
-        {
-            Console.WriteLine("That's not a whole number. Enter a whole number:");
-            num = Console.ReadLine();
-        }
+		while (!(Int32.TryParse(num, out int number)))
+		{
+			Console.WriteLine("That's not a whole number. Enter a whole number:");
+			num = Console.ReadLine();
+		}
 
-        return num;
-    }
+		return num;
+	}
 
-    public String getNumber2(String origNum)
-    {
-        String num;
+	public String getNumber2(String origNum)
+	{
+		String num;
 
-        Console.WriteLine("Enter another whole number with " + origNum.Length + " digits:");
-        num = Console.ReadLine();
+		Console.WriteLine("Enter another whole number with {0} digits:", origNum.Length);
+		num = Console.ReadLine();
 
-        while (!(Int32.TryParse(num, out int number)))
-        {
-            Console.WriteLine("That's not a whole number. Enter a whole number with " + origNum.Length + " digits:");
-            num = Console.ReadLine();
-        }
+		while (!(Int32.TryParse(num, out int number)))
+		{
+			Console.WriteLine("That's not a whole number. Enter a whole number with {0} digits:", origNum.Length);
+			num = Console.ReadLine();
+		}
 
 
-        while (num.Length != origNum.Length)
-        {
-            Console.WriteLine("No, enter a whole number with " + origNum.Length + " digits:");
-            num = Console.ReadLine();
-        }
+		while (num.Length != origNum.Length)
+		{
+			Console.WriteLine("No, enter a whole number with {0} digits:", origNum.Length);
+			num = Console.ReadLine();
+		}
 
-        return num;
-    }
+		return num;
+	}
 
-    static public void Main()
-    {
-        String num1, num2;
+	static public void Main()
+	{
+		NumberChecker n = new NumberChecker();
 
-        NumberChecker n = new NumberChecker();
+		String num1 = n.getNumber(), num2 = n.getNumber2(num1);
 
-        num1 = n.getNumber();
-        num2 = n.getNumber2(num1);
+		int initialTotal = Convert.ToInt32(num1[0]) + Convert.ToInt32(num2[0]);
 
-        int initialTotal = Convert.ToInt32(num1[0]) + Convert.ToInt32(num2[0]);
+		for (int i = 1; i < num1.Length; i++)
+		{
+			if (Convert.ToInt32(num1[i]) + Convert.ToInt32(num2[i]) == initialTotal)
+			{
+				if (i == num1.Length - 1)
+				{
+					Console.WriteLine(true);
+				}
+			} else
+			{
+				Console.WriteLine(false);
+				break;
+			}
 
-        for (int i = 1; i < num1.Length; i++)
-        {
-            if (Convert.ToInt32(num1[i]) + Convert.ToInt32(num2[i]) == initialTotal)
-            {
-                if (i == num1.Length - 1)
-                {
-                    Console.WriteLine(true);
-                }
-            } else
-            {
-                Console.WriteLine(false);
-                break;
-            }
+		}
 
-        }
-
-        Console.ReadKey();
-    }
+		Console.ReadKey();
+	}
 }
